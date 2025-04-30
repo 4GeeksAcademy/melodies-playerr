45aed43771a7d6adbef37373145d0a8836c3bb34
KaitoKidd86 <liritokun1@gmail.com>
2025-04-30 21:34:27 +0000

    spotifyplayer

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [songs, setSongs] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(null);
+    const audioRef = useRef();
 
-export default Home;
+    const obtenerCanciones = async () => {
+        try {
+            const response = await fetch("https://playground.4geeks.com/sound/songs")
+            console.log(response)
+            const data = await response.json()
+            console.log(data.songs)
+            setSongs(data.songs)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const reproducir = (url, id) => {
+        let apiSongs = "https://playground.4geeks.com"
+
+        if (audioRef.current.paused) {
+
+            audioRef.current.src = apiSongs.concat(url)
+            audioRef.current.play()
+        }
+    }
+
+    useEffect(() => {
+        obtenerCanciones()
+    }, [])
+
+
+
+    return (
+        <div className="container text-center">
+            <h1>mi spotify</h1>
+            <div className="d-flex justify-content-center">
+                <div class="list-group">
+                    {songs.map((cancion) => (
+                        <button type="button" value={cancion} onClick={() => reproducir(cancion.url, cancion.id)} class="list-group-item list-group-item-action " aria-current="true">
+                            {cancion.name}
+                        </button>
+                    ))}
+
+                </div>
+            </div>
+            <audio ref={audioRef} controls>
+                <source src={audioRef} type="audio/mp3" />
+            </audio>
+        </div>
+    )
+}
+export default Home
